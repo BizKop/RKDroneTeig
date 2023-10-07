d1b9574692f60033d771ece49e5e9c7750934ee6
BizKop <hans.erik.ruud@gmail.com>
2023-10-07 16:19:40 +0200

    Update index.es.js

--- index.es.js
+++ index.es.js
@@ -160,13 +160,14 @@ function fromLatLon(latitude, longitude, forceZoneNum) {
                                          a4 / 24 * (5 - latTan2 + 9 * c + 4 * c * c) +
                                          a6 / 720 * (61 - 58 * latTan2 + latTan4 + 600 * c - 330 * E_P2)));
   if (latitude < 0) northing += 1e7;
-
-  return {
-    easting: easting,
-    northing: northing,
-    zoneNum: zoneNum,
-    zoneLetter: zoneLetter
-  };
+  var returnValue = zoneNum + zoneLetter + "0" + toString(easting).substring(0,6) + "E " + toString(northing).substring(0,7) + "N";
+  return returnValue;
+  // return {
+    // easting: easting,
+    // northing: northing,
+    // zoneNum: zoneNum,
+    // zoneLetter: zoneLetter
+  // };
 }
 
 function latitudeToZoneLetter(latitude) {
