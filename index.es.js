d891d610ba5f4d26dd2f7f9683637d368eace9c9
BizKop <hans.erik.ruud@gmail.com>
2023-10-07 16:36:24 +0200

    Update index.es.js

--- index.es.js
+++ index.es.js
@@ -160,7 +160,7 @@ function fromLatLon(latitude, longitude, forceZoneNum) {
                                          a4 / 24 * (5 - latTan2 + 9 * c + 4 * c * c) +
                                          a6 / 720 * (61 - 58 * latTan2 + latTan4 + 600 * c - 330 * E_P2)));
   if (latitude < 0) northing += 1e7;
-  var returnValue = zoneNum + zoneLetter + "0" + toString(easting) + "E " + toString(northing) + "N";
+  var returnValue = zoneNum + zoneLetter + "0" + easting.toString() + "E " + northing.toString() + "N";
   return returnValue;
   // return {
     // easting: easting,
